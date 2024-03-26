2c1c4cd4aabd04d858d8127c76df6e8eb099387b
Aleksei <ach1010110@yandex.ru>
2024-03-26 19:05:51 +0300

    first solution. bad for time and resources

--- task_sort_data_from_sensors_from_yt_video.py
+++ task_sort_data_from_sensors_from_yt_video.py
@@ -1,52 +1,24 @@
-sensor1_data = [
-    "2024-03-191 datafromsensor2221 veryimportentintdata1",
-    "2024-43-211 datafromsensor2221 veryimportentintdata56465487981631684964",
-    "2024-23-11 datafromsensor2221 veryimportentintdata564654879816316845964",
-    "2024-03-191 datafromsensor2221 veryimportentintdata5646548798163163454964",
-    "2024-03-41 datafromsensor2221 veryimportentintdata56465487981631686434564",
-    "2024-03-101 datafromsensor2221 veryimportentintdata56465487981631684964",
-    "2024-03-31 datafromsensor2221 veryimportentintdata56465487981631684964",
-    "2024-03-10 datafromsensor2221 veryimportentintdata56465487981631684964",
-    "2024-03-191 datafromsensor2221 veryimportentintdata2",
-    "2024-03-13 datafromsensor2221 veryimportentintdata5646548798163164584964",
-    "2024-03-41 datafromsensor2221 veryimportentintdata56465487981631684964",
-    "2024-03-81 datafromsensor2221 veryimportentintdata56465487981631684964",
-]
-sensor2_data = [
-    "2024-053-191 datafromsensor555 veryimportentintdata56465487981631683454964",
-    "2024-423-211 datafromsensor555 veryimportentintdata56465487981631684964",
-    "2024-243-11 datafromsensor555 veryimportentintdata56465487981631684345964",
-    "2024-503-121 datafromsensor555 veryimportentintdata56465487981631684964",
-    "2024-03-191 datafromsensor555 veryimportentintdata64",
-    "2024-03-01 datafromsensor555 veryimportentintdata56465487981631684964",
-    "2024-03-31 datafromsensor555 veryimportentintdata56465487981631683454964",
-    "2024-01-123 datafromsensor555 veryimportentintdata56465487981631684964",
-    "2024-14-11 datafromsensor555 veryimportentintdata56465487981631684964",
-    "2024-03-191 datafromsensor555 veryimportentintdata56465487981631684964",
-    "2024-63-41 datafromsensor555 veryimportentintdata5646548798163163454964",
-    "2024-43-81 datafromsensor555 veryimportentintdata56465487981631684964",
-]
+import random
+import datetime
+# prepare data for task
+DATA_COUNT = 5
+
+data = [[(str(random.randint(1000000000, 10000000000)) +
+        ' neurodivergence.altitude.z0 ' + str(random.randint(1, 1000000))) for x in range(DATA_COUNT)] for i in range(DATA_COUNT)]
 
 
-def combine_sensors_data(list1: list, list2: list) -> list:
-    # total_data = list1 + list2
-    # total_data_list = []
-
-    # for rec in total_data:
-    #     data = rec.split(' ')
-    #     data_rec = {
-    #         'time': data[0],
-    #         'data1': data[1],
-    #         'data2': data[2]
-    #     }
-    #     total_data_list.append(data_rec)
-
-    # return sorted(total_data_list, key=lambda x: x['time'])
-
-    return sorted(list1 + list2, key=lambda x: (x.split(' ')[0], x.split(' ')[2]))
+def combine_sensors_data(*args: list) -> list:
+    all_lists = []
+    for arg in args:
+        all_lists.extend(arg)
+    return sorted(all_lists, key=lambda x: (int(x.split(' ')[0]), int(x.split(' ')[2])))
 
 
-res = combine_sensors_data(sensor2_data, sensor1_data)
-# print(res)
-for x in res:
+# Solution #1 time
+time1 = datetime.datetime.now()
+res1 = combine_sensors_data(*data)
+delta_time = datetime.datetime.now() - time1
+print(f'Processing time solution #1 is {delta_time} seconds')
+
+for x in res1:
     print(x)
