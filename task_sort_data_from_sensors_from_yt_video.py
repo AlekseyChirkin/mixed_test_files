8bd688c31fd82b703223d11c0379f9d3ec6ca475
Aleksei <ach1010110@yandex.ru>
2024-03-26 19:09:52 +0300

    prepare for second solution

--- task_sort_data_from_sensors_from_yt_video.py
+++ task_sort_data_from_sensors_from_yt_video.py
@@ -15,11 +15,21 @@ def combine_sensors_data(*args: list) -> list:  # Solution #1
     return sorted(all_lists, key=lambda x: (int(x.split(' ')[0]), int(x.split(' ')[2])))
 
 
+def combine_sensors_data2(*args: list) -> list:  # Solution #2
+    return args
+
+
 # Solution #1 time
 time1 = datetime.datetime.now()
 res1 = combine_sensors_data(*data)
 delta_time = datetime.datetime.now() - time1
 print(f'Processing time solution #1 is {delta_time} seconds')
+
+# Solution #2 time
+time1 = datetime.datetime.now()
+res1 = combine_sensors_data2(*data)
+delta_time = datetime.datetime.now() - time1
+print(f'Processing time solution #2 is {delta_time} seconds')
 
 for x in res1:
     print(x)
