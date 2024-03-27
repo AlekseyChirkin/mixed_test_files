23cacae6b443d1e4aadc288e9db3794001a28719
AlekseyChirkin <ach1010110@yandex.ru>
2024-03-27 17:29:48 +0300

    add second function for find most common int in list

--- find_freq.py
+++ find_freq.py
@@ -6,8 +6,18 @@ def count_frequency(lst: list):
     return sorted(dict(Counter(lst)).items(), key=lambda item: item[1])
 
 
+def count_frequency2(lst: list):
+    return Counter(lst).most_common(1)
+
+
 elements = [random.randint(1, 100) for x in range(100)]
 freq = count_frequency(elements)
-most_freq = freq[-1][0]
+most_freq = freq[-1]
+print(f"""
+      {elements=}\n
+      {freq=}\n
+      Most frequent element by "sorted and lambda function" is {most_freq}
+      """)
 
-print(f"{elements=}\n{freq=}\nMost frequent element is {most_freq}")
+freq = count_frequency2(elements)
+print(f'Most frequent element by "most_common" function is {freq}')
